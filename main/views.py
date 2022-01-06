2fe04406b5b661cb67b34b7608ded6abec13ad84
Ashkan <AshkanGanj@gmail.com>
2022-01-06 17:33:56 +0330

    deploy djagno on landing and restaurant pages

---
+++ main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import Http404
+
+def index(request):
+
+    return render(request, 'index.html', {})
+
+
+def restaurant(request):
+
+    return render(request, 'resturantListPage.html', {})
+
+
